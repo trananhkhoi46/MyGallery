571d61866cb54ef5042523d9bc894b9d6835a948
khoi tran <khoitran@misfit.com>
2016-11-18 22:55:57 +0700

    Update android mk

--- MyGalleryGame/proj.android/jni/Android.mk
+++ MyGalleryGame/proj.android/jni/Android.mk
@@ -16,6 +16,7 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
 ../../Classes/AppDelegate.cpp \
 ../../Classes/HomeScene.cpp \
 ../../Classes/BaseScene.cpp \
+../../Classes/SettingScene.cpp \
 ../../Classes/SplashScene.cpp
 
 LOCAL_CPPFLAGS := -DSDKBOX_ENABLED
